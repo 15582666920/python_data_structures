48fb1b1dc6d8095054e66ca78ccc86e61c9375a6
sunbo <799664881@qq.com>
2020-03-06 16:06:27 +0800

    '可视化递归，汉诺塔游戏'

--- ds2-recursion/listsum.py
+++ ds2-recursion/listsum.py
@@ -2,7 +2,6 @@
 [1,3,5,7,9]
 """
 
-
 #
 # def listSum(numList):
 #     sum = 0
@@ -25,12 +24,33 @@
 #
 # print(listSum2([1, 3, 5, 7, 9 ]))
 
-def tostr(n, base):
-    str1 = '0123456789ABCDEF'
-    if n < base:
-        return str1[n]
-    else:
-        return tostr(n // base, base) + str1[n % base]
+# def tostr(n, base):
+#     str1 = '0123456789ABCDEF'
+#     if n < base:
+#         return str1[n]
+#     else:
+#         return tostr(n // base, base) + str1[n % base]
+#
+#
+# print(tostr(7, 2))
+from pythonds.basic.stack import Stack
+
+rStack = Stack()
 
 
-print(tostr(7, 2))
+def toStr(n, base):
+    converString = '0123456789ABCDEF'
+
+    while n > 0:
+        if n < base:
+            rStack.push(converString[n])
+        else:
+            rStack.push(converString[n % base])
+        n = n // base
+    res = ''
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
+
+
+print(toStr(7, 2))
